4a67b7a6d69c59ac085d6f7f85763b5760e8a4e3
Fer <fernando.falcon@holbertonstudents.com>
2024-12-10 19:46:18 +0000

    Is node a leaf

--- binary_trees.h
+++ binary_trees.h
@@ -23,9 +23,10 @@ typedef struct binary_tree_s
 void binary_tree_print(const binary_tree_t *);
 
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *, int);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *, int);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *, int);
+void binary_tree_delete(binary_tree_t *);
+int binary_tree_is_leaf(const binary_tree_t *);
 
 #endif
